9b602d58ffd7c1fd896d276ee1cfd6a1e004485c
santino <bournotsantino03@gmail.com>
2022-09-17 12:40:02 -0300

    Ejercicios del segundo cuatrimestre subidos al repositorio

--- Ejercicios_Programación/2do_Cuatrimestre/Practica_4/Ejercicio_2/src/App.java
+++ Ejercicios_Programación/2do_Cuatrimestre/Practica_4/Ejercicio_2/src/App.java
@@ -11,21 +11,14 @@ public class App {
         try (Scanner leer = new Scanner(System.in)) {
             System.out.println("Ingresa una frase");
             String frase = leer.nextLine();
-            int k = frase.length();
 
-            char[] salida = new char[k];
-
-            for (int i = 0; i < salida.length; i++) {
-                salida[i] = frase.charAt(i);
-            }
+            char[] salida = frase.toCharArray();
 
             System.out.println("La frase elegida es: " + frase + "\nPasado a char:");
 
             for (char j : salida) {
                 System.out.println(j);
             }
-                
-            
         }
     }
 }
