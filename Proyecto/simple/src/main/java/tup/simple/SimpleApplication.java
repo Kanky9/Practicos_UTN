ca66cc10b6d96a1a21c5db6fa20f9390ed344d87
santino <bournotsantino03@gmail.com>
2022-10-18 01:06:21 -0300

    Proyecto UTN

---
+++ Proyecto/simple/src/main/java/tup/simple/SimpleApplication.java
@@ -0,0 +1,17 @@
+package tup.simple;
+
+/*
+Esta es la clase principal de la aplicación.
+Vemos que solo tiene las anotaciones de SpringBoot.
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApplication.class, args);
+    }
+
+}
